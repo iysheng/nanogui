fa273a66bfa33ccff7aa36cce25f40f71fef2683
iysheng <iysheng@163.com>
2022-08-30 08:42:10 +0800

    [ADD][N/A] 添加同步 link.txt 的脚本

---
+++ sync_link.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+PWD=`pwd`
+BUILDDIR=
+
+cp ../link.txt_ledmain $PWD/$BUILDDIR/CMakeFiles/ledmain.dir/link.txt -frv
+cp /tmp/link.txt $PWD/$BUILDDIR/CMakeFiles/example1.dir/link.txt -frv
