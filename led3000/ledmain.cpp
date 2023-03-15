9624abf34f65d63c85cc31e7032f0c983944fbee
iysheng <iysheng@163.com>
2023-03-15 09:11:13 +0800

    [CHG][N/T] 添加 BACKTRACE_DEBUG 控制出错处理

--- led3000/ledmain.cpp
+++ led3000/ledmain.cpp
@@ -15,6 +15,12 @@
 #include <thread>
 #include <unistd.h>
 #include <debug.h>
+#include <sys/prctl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <errno.h>
 
 using std::cout;
 using std::cerr;
@@ -30,9 +36,78 @@ extern void *joystick_thread(void *arg);
 using namespace nanogui;
 using namespace rapidjson;
 
+#define BACKTRACE_DEBUG
+#ifdef BACKTRACE_DEBUG
+#include <execinfo.h>
+
+typedef void (*signal_func4trace_t)(int sig, siginfo_t *info, void *secret);
+
+static void signal_func4trace(int sig, siginfo_t *info, void *secret)
+{
+#define BT_BUF_SIZE 100
+    int j, nptrs;
+    void *buffer[BT_BUF_SIZE];
+    char pthread_name[16] = {0};
+    char **strings;
+
+    nptrs = backtrace(buffer, BT_BUF_SIZE);
+    printf("backtrace() returned %d addresses sig=%d\n", nptrs, sig);
+
+    /* The call backtrace_symbols_fd(buffer, nptrs, STDOUT_FILENO)
+       would produce similar output to the following: */
+
+    strings = backtrace_symbols(buffer, nptrs);
+    if (strings == NULL)
+    {
+        perror("backtrace_symbols");
+        exit(EXIT_FAILURE);
+    }
+
+    for (j = 0; j < nptrs; j++)
+        printf("%s\n", strings[j]);
+
+    free(strings);
+    if (!prctl(PR_GET_NAME, pthread_name))
+        printf("thread_name=%s\n", pthread_name);
+    else
+        perror("failed to get thread name\n");
+
+    exit(0);
+}
+
+static int init_backtrace(signal_func4trace_t func4trace)
+{
+    struct sigaction action4backtrace = {0};
+
+    action4backtrace.sa_sigaction = func4trace;
+    sigemptyset(&action4backtrace.sa_mask);
+    action4backtrace.sa_flags = SA_RESTART | SA_SIGINFO;
+    sigaction(SIGINT, &action4backtrace, NULL);
+    sigaction(SIGILL, &action4backtrace, NULL);
+    sigaction(SIGABRT, &action4backtrace, NULL);
+    sigaction(SIGFPE, &action4backtrace, NULL);
+    sigaction(SIGSEGV, &action4backtrace, NULL);
+    sigaction(SIGTERM, &action4backtrace, NULL);
+
+    sigaction(SIGQUIT, &action4backtrace, NULL);
+    sigaction(SIGTRAP, &action4backtrace, NULL);
+    sigaction(SIGKILL, &action4backtrace, NULL);
+    sigaction(SIGSYS, &action4backtrace, NULL);
+    sigaction(SIGBUS, &action4backtrace, NULL);
+    sigaction(SIGSYS, &action4backtrace, NULL);
+
+    return 0;
+}
+
+#endif
+
+
 int g_test_btn_len;
 int main(int /* argc */, char ** /* argv */)
 {
+#ifdef BACKTRACE_DEBUG
+    init_backtrace(&signal_func4trace);
+#endif
     RedDebug::init("/tmp/ledmain_log.txt");
     RedDebug::print_soft_info();
     /* 创建了测试窗口类 */
