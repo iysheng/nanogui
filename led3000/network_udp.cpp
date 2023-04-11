dd558aedba826fb85f7a6433a20b366a44249f8c
iysheng <iysheng@163.com>
2023-04-11 19:03:15 +0800

    [CHG][N/T] 修改 udp 为阻塞型以及添加接收超时为 5s

--- led3000/network_udp.cpp
+++ led3000/network_udp.cpp
@@ -43,42 +43,45 @@ int NetworkUdp::try_to_connect(void)
         return -1;
     }
 
-    m_socket = socket(m_addrinfo->ai_family, SOCK_DGRAM | SOCK_CLOEXEC, IPPROTO_UDP);
+    m_socket = socket(m_addrinfo->ai_family, SOCK_DGRAM, IPPROTO_UDP);
     if (m_socket == -1) {
         //RedDebug::log("could not retry create udp socket\n");
         return -2;
     }
 #ifdef SOCKET_NO_BLOCK
     struct timeval tv;
-    tv.tv_sec = 3;
+    tv.tv_sec = 5;
     tv.tv_usec = 0;
     /* 设置接收超时 */
     setsockopt(m_socket, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
 #endif
 
+    /* 开启多线程复用 */
+    int32_t reuse_enable = 1;
+    if (setsockopt(m_socket, SOL_SOCKET, SO_REUSEPORT, &(reuse_enable), sizeof(reuse_enable)) < 0) {
+        RedDebug::err("set reuse_enable %u error:%d", reuse_enable, errno);
+        close(m_socket);
+        m_socket = -1;
+        return -4;
+    }
+
     int r;
     r = bind(m_socket, (struct sockaddr *) &m_source_sin, sizeof m_source_sin);
-    if (r != 0 && errno != EADDRINUSE) {
+    if (r == -1) {
         close(m_socket);
         m_socket = -1;
         RedDebug::err("Red could not try to bind UDP socket with port err=%d addinuse:%d", errno, EADDRINUSE);
         return -3;
-    }
-    else if (errno == EADDRINUSE && NetworkUdp::gs_socket_udp_alone_fd > 0)
-    {
-        close(m_socket);
-        m_socket = NetworkUdp::gs_socket_udp_alone_fd;
-        red_debug_lite("Use gs_socket_udp_alone_fd instead");
     }
     else if (0 == r)
     {
         /* 根据对方要求修改 TTL */
         char ttl = 8;
         if (setsockopt(m_socket, IPPROTO_IP, IP_MULTICAST_TTL, (char *)&ttl, sizeof(ttl)) != 0) {
-            RedDebug::log("Failed set ttl to %d err:%d", ttl, errno);
+            RedDebug::err("Failed set ttl to %d err:%d", ttl, errno);
             close(m_socket);
             m_socket = -1;
-            return -4;
+            return -5;
         }
     }
 
@@ -90,26 +93,22 @@ int NetworkUdp::try_to_connect(void)
     req.imr_interface.s_addr = inet_addr("168.9.0.1");//本地地址
     ret  = setsockopt(m_socket, IPPROTO_IP, IP_ADD_MEMBERSHIP, &(req), sizeof(req));//IPPROTO_IP通过IP组播，IP_ADD_MEMBERSHIP -> 加入组播组
     if (ret < 0) {
-        RedDebug::log("add multi to %s error:%d", inet_ntoa(dstip_in_addr), errno);
+        RedDebug::err("set multi to %s error:%d", inet_ntoa(dstip_in_addr), errno);
         close(m_socket);
         m_socket = -1;
-        return -5;
-    } else {
-        NetworkUdp::gs_socket_udp_alone_fd = m_socket;
-        RedDebug::log("add multi to %s success", inet_ntoa(dstip_in_addr));
+        return -6;
     }
 
     /* 禁止环回 */
     uint32_t loop_enable = 0;
     ret  = setsockopt(m_socket, IPPROTO_IP, IP_MULTICAST_LOOP, &(loop_enable), sizeof(loop_enable));
     if (ret < 0) {
-        RedDebug::log("disable multi loop_enable %u error:%d", loop_enable, errno);
+        RedDebug::err("set multi loop_enable %u error:%d", loop_enable, errno);
         close(m_socket);
         m_socket = -1;
-        return -6;
+        return -7;
     } else {
-        NetworkUdp::gs_socket_udp_alone_fd = m_socket;
-        RedDebug::log("disable multi loop_enable to %u success", loop_enable);
+        RedDebug::log("set multi loop_enable to %u success", loop_enable);
     }
 
     return m_socket;
@@ -140,7 +139,7 @@ NetworkUdp::NetworkUdp(string dstip, uint16_t source_port, uint16_t dst_port, in
         RedDebug::log("use origin socket fd:%d", m_socket);
     } else {
         /* 默认都是阻塞型的 */
-        m_socket = socket(m_addrinfo->ai_family, SOCK_DGRAM | SOCK_CLOEXEC, IPPROTO_UDP);
+        m_socket = socket(m_addrinfo->ai_family, SOCK_DGRAM, IPPROTO_UDP);
         if (m_socket == -1) {
             freeaddrinfo(m_addrinfo);
             red_debug_lite("could not create socket address or port: %s:%u\n", dstip.c_str(), dst_port);
@@ -148,11 +147,23 @@ NetworkUdp::NetworkUdp(string dstip, uint16_t source_port, uint16_t dst_port, in
         }
 #ifdef SOCKET_NO_BLOCK
         struct timeval tv;
-        tv.tv_sec = 3;
+        tv.tv_sec = 5;
         tv.tv_usec = 0;
         /* 设置接收超时 */
         setsockopt(m_socket, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
 #endif
+
+        /* 开启多线程复用 */
+        int32_t reuse_enable = 1;
+        if (setsockopt(m_socket, SOL_SOCKET, SO_REUSEPORT, &(reuse_enable), sizeof(reuse_enable)) < 0) {
+            RedDebug::err("set reuse_enable %u error:%d", reuse_enable, errno);
+            close(m_socket);
+            m_socket = -1;
+            return;
+        } else {
+            RedDebug::log("set reuse_enable to %u success", reuse_enable);
+        }
+
         m_source_sin.sin_addr.s_addr = htonl(INADDR_ANY);//inet_addr(dstip.c_str());
         m_source_sin.sin_family = AF_INET;
         m_source_sin.sin_port = htons(source_port);
@@ -164,10 +175,11 @@ NetworkUdp::NetworkUdp(string dstip, uint16_t source_port, uint16_t dst_port, in
             red_debug_lite("could not bind UDP socket with port when contruct:%u\n", source_port);
             return;
         }
+
         /* 根据对方要求修改 TTL */
         char ttl = 8;
         if (setsockopt(m_socket, IPPROTO_IP, IP_MULTICAST_TTL, (char *)&ttl, sizeof(ttl)) != 0) {
-            RedDebug::log("Failed set ttl to %d err:%d", ttl, errno);
+            RedDebug::err("Failed set ttl to %d err:%d", ttl, errno);
             close(m_socket);
             m_socket = -1;
             return;
@@ -179,29 +191,25 @@ NetworkUdp::NetworkUdp(string dstip, uint16_t source_port, uint16_t dst_port, in
     int ret;
     struct ip_mreq req = {0};//结构体对象
     req.imr_multiaddr.s_addr = inet_addr(dstip.c_str());//设置组播地址
-    req.imr_interface.s_addr = INADDR_ANY;//本地地址
+    req.imr_interface.s_addr = inet_addr("168.9.0.1");//本地地址
     ret  = setsockopt(m_socket, IPPROTO_IP, IP_ADD_MEMBERSHIP, &(req), sizeof(req));//IPPROTO_IP通过IP组播，IP_ADD_MEMBERSHIP -> 加入组播组
     if (ret < 0) {
-        RedDebug::log("add multi to %s error:%d", dstip.c_str(), errno);
+        RedDebug::err("add multi to %s error:%d", dstip.c_str(), errno);
         close(m_socket);
         m_socket = -1;
         return;
-    } else {
-        NetworkUdp::gs_socket_udp_alone_fd = m_socket;
-        RedDebug::log("add multi to %s success", dstip.c_str());
     }
 
     /* 禁止环回 */
     uint32_t loop_enable = 0;
     ret  = setsockopt(m_socket, IPPROTO_IP, IP_MULTICAST_LOOP, &(loop_enable), sizeof(loop_enable));
     if (ret < 0) {
-        RedDebug::log("disable multi loop_enable %u error:%d", loop_enable, errno);
+        RedDebug::log("set loop_enable %u error:%d", loop_enable, errno);
         close(m_socket);
         m_socket = -1;
         return;
     } else {
-        NetworkUdp::gs_socket_udp_alone_fd = m_socket;
-        RedDebug::log("disable multi loop_enable to %u success", loop_enable);
+        RedDebug::log("set loop_enable to %u success", loop_enable);
     }
 }
 
@@ -215,8 +223,9 @@ int NetworkUdp::send2server(char *buffer, uint16_t len, int flags)
     }
     ret = sendto(m_socket, buffer, len, flags, m_addrinfo->ai_addr, m_addrinfo->ai_addrlen);
     if (-1 == ret) {
-        RedDebug::log("Send msg to server %s failed:%d\n",
+        RedDebug::err("Send msg to server %s failed:%d\n",
                       inet_ntoa(((sockaddr_in *)m_addrinfo->ai_addr)->sin_addr), errno);
+        errno = 0;
     } else {
         //RedDebug::log("Send msg to server %s success\n", inet_ntoa(((sockaddr_in *)m_addrinfo->ai_addr)->sin_addr));
         RedDebug::hexdump("UDP SEND", (char *)buffer, len);
@@ -238,8 +247,9 @@ int NetworkUdp::send2server(char *buffer, uint16_t len, int flags, struct addrin
     }
     ret = sendto(m_socket, buffer, len, flags, addr->ai_addr, addr->ai_addrlen);
     if (-1 == ret) {
-        RedDebug::log("Send msg to server %s failed:%d\n",
+        RedDebug::err("Send msg to server %s failed:%d\n",
                       inet_ntoa(((sockaddr_in *)addr->ai_addr)->sin_addr), errno);
+        errno = 0;
     } else {
         //RedDebug::log("Send msg to server %s success\n", inet_ntoa(((sockaddr_in *)addr->ai_addr)->sin_addr));
         RedDebug::hexdump("UDP SEND", (char *)buffer, len);
@@ -258,10 +268,16 @@ int NetworkUdp::recv_from_server(char *buffer, uint16_t len, int flags)
         //RedDebug::log("invalid udp socket and try to connect server failed");
         return -1;
     }
-    //RedDebug::log("before udp socket recv:%s %p", inet_ntoa(((sockaddr_in *)m_addrinfo->ai_addr)->sin_addr), m_addrinfo);
     ret = recvfrom(m_socket, buffer, len, flags, NULL, NULL);
     if (-1 == ret) {
         RedDebug::log("Failed recvfrom server :%d %s %p", errno, inet_ntoa(((sockaddr_in *)m_addrinfo->ai_addr)->sin_addr), m_addrinfo);
+        if (errno == EAGAIN)
+        {
+            /* mark socket invalid */
+            close(m_socket);
+            m_socket = -1;
+        }
+        errno = 0;
     } else if (ret > 0) {
         RedDebug::hexdump("RECV_FROM_SEREVR", buffer, ret);
     }
