b0ab8a6160f6b5690f9c02d6cacbf4d72c3896ae
iysheng <iysheng@163.com>
2023-05-16 18:18:23 +0800

    [CHG][N/T] 修改版本号相关内容

--- led3000/version.h
+++ led3000/version.h
@@ -2,9 +2,9 @@
 #define __VERSION_H__
 
 #define LED3000_MAJOR_VERSION 1
-#define LED3000_MINOR_VERSION 0
-#define LED3000_PATCH_VERSION 5
-#define LED3000_VERSION (1 << 16 | 0 << 8 | 5)
-#define LED3000_COMMIT_ID     "f1a5e77"
+#define LED3000_MINOR_VERSION 1
+#define LED3000_PATCH_VERSION 1
+#define LED3000_VERSION (1 << 16 | 1 << 8 | 1)
+#define LED3000_COMMIT_ID     "d4b1015"
 
 #endif /* ifndef __VERSION_H__ */
