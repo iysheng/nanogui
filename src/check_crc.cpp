86e72c215987d1095d1f4a07538e5626c85db9f2
iysheng <iysheng@163.com>
2023-05-19 15:15:34 +0800

    [CHG][N/T] 添加 CRC 校验小工具代码

---
+++ src/check_crc.cpp
@@ -0,0 +1,47 @@
+/*
+    src/example1.cpp -- C++ version of an example application that shows
+    how to use the various widget classes. For a Python implementation, see
+    '../python/example1.py'.
+
+    NanoGUI was developed by Wenzel Jakob <wenzel.jakob@epfl.ch>.
+    The widget drawing code is based on the NanoVG demo application
+    by Mikko Mononen.
+
+    All rights reserved. Use of this source code is governed by a
+    BSD-style license that can be found in the LICENSE.txt file.
+*/
+
+#include "check_crc.h"
+#include <thread>
+
+void create_backend_threads(RedBurntool *app)
+{
+};
+
+int main(int /* argc */, char ** /* argv */) {
+    try {
+        nanogui::init();
+
+        /* scoped variables */ {
+            ref<RedBurntool> app = new RedBurntool();
+            app->dec_ref();
+            app->draw_all();
+            app->set_visible(true);
+            nanogui::mainloop(1 / 60.f * 1000);
+        }
+
+        nanogui::shutdown();
+    } catch (const std::exception &e) {
+        std::string error_msg = std::string("Caught a fatal error: ") + std::string(e.what());
+        #if defined(_WIN32)
+            MessageBoxA(nullptr, error_msg.c_str(), NULL, MB_ICONERROR | MB_OK);
+        #else
+            std::cerr << error_msg << std::endl;
+        #endif
+        return -1;
+    } catch (...) {
+        std::cerr << "Caught an unknown error!" << std::endl;
+    }
+
+    return 0;
+}
